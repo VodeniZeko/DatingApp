5066f064ca84f2933ac05293842431fa2abcf706
VodeniZeko <saletovic.developer@gmail.com>
2024-05-25 20:56:20 -0500

    add login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,26 @@ public class AccountController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")] //POST: api/account/login
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+        if (user == null) return Unauthorized("invalid username");
+
+        using var hmac = new HMACSHA512(user.PassSalt);
+
+        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computeHash.Length; i++)
+        {
+            if (computeHash[i] != user.PassHash[i]) return Unauthorized("invalid password");
+        }
+
+        return user;
+    }
+
+
     private async Task<bool> UserExist(string username)
     {
         return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace API;
+
+public class LoginDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
