40e8bbc757f4957d1c9f4fdf306ac0c6ca2550be
VodeniZeko <saletovic.developer@gmail.com>
2024-05-25 23:20:41 -0500

    use extension services in program

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
-
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
