bc6de17ef2db6cc92f0f73655195830dc238777a
VodeniZeko <saletovic.developer@gmail.com>
2024-07-11 13:47:26 -0500

    update user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Entities;
+﻿using API.Extensions;
+
+namespace API.Entities;
 
 public class AppUser
 {
@@ -6,4 +8,20 @@ public class AppUser
     public string UserName { get; set; } = string.Empty;
     public byte[] PassHash { get; set; }
     public byte[] PassSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; } = string.Empty;
+    public string Introduction { get; set; } = string.Empty;
+    public string LookingFor { get; set; } = string.Empty;
+    public string Interests { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public List<Photo> Photos { get; set; } = new List<Photo>();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
