f2a867abf5ca83184287d8ef318878884212d624
VodeniZeko <saletovic.developer@gmail.com>
2024-07-10 20:23:03 -0500

    add a controller to test exceptions

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,49 @@
+﻿using API.Controllers;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class BuggyController : BaseApiController
+{
+  private readonly DataContext _context;
+  public BuggyController(DataContext context)
+  {
+    _context = context;
+  }
+
+  [Authorize]
+  [HttpGet("auth")]
+  public ActionResult<string> GetSecret()
+  {
+    return "secret text";
+  }
+
+  [HttpGet("not-found")]
+  public ActionResult<AppUser> GetNotFound()
+  {
+    var test = _context.Users.Find(-1);
+
+    if (test == null) return NotFound();
+
+    return test;
+  }
+
+  [HttpGet("server-error")]
+  public ActionResult<string> GetServerError()
+  {
+    var test = _context.Users.Find(-1);
+
+    var thingToReturn = test.ToString();
+
+    return thingToReturn;
+  }
+
+  [HttpGet("bad-request")]
+  public ActionResult<string> GetBadRequest()
+  {
+    return BadRequest("this is a bad request");
+  }
+}
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,5 +7,6 @@ public class RegisterDto
     [Required]
     public string UserName { get; set; } = String.Empty;
     [Required]
+    [StringLength(8, MinimumLength = 4)]
     public string Password { get; set; } = String.Empty;
 }
